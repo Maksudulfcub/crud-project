56313b9d2902f641d4a941e07b7d27ff4c70490c
Maksudulfcub <maaksuduldish@gmail.com>
2025-01-08 12:27:42 +0600

    Showing all data from database to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,12 +22,65 @@
             <h1 class="text-2xl font-bold">Home</h1>
             <a class="bg-sky-600 text-white px-4 py-1 rounded-sm" href="/create">Add New Post</a>
         </div>
-        <h1 class="text-7xl font-bold text-center mt-20">CRUD Project</h1>
-        <h1 class="text-2xl text-gray-600 font-medium text-center mt-20">Create, Read, Update & Delete your data easily !</h1>
-        
+        <h1 class="text-5xl font-bold text-center">CRUD Project</h1>
+        <h1 class="text-lg text-gray-600 font-medium text-center">Create, Read, Update & Delete your data easily
+            !</h1>
+
         @if (session('success'))
             <h2 class="text-3xl text-green-600 font-medium text-center mt-20">{{ session('success') }}</h2>
         @endif
+
+        <div class="mt-10">
+            <div class="flex flex-col bg-white">
+                <div class="-m-1.5 overflow-x-auto">
+                    <div class="p-1.5 min-w-full inline-block align-middle">
+                        <div class="overflow-hidden">
+                            <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                                <thead>
+                                    <tr>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                            ID</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                            NAME</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                            DESCRIPTION</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                            IMAGE</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                            ACTION</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($posts as $post)
+                                        <tr class="bg-white">
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                                {{ $post->id }}</td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                                {{ $post->name }}</td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                                {{ $post->description }}</td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm">
+                                                <img src="images/{{ $post->image }}" width="30px" alt="">
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                                                <button type="button"
+                                                    class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400">Delete</button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
